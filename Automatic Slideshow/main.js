1159f04aefffb519dfe39b0a6e36ca07745546a9
sarbanandadev <sarbanandadev@gmail.com>
2021-11-19 20:00:58 +0600

    initial commit

---
+++ Automatic Slideshow/main.js
@@ -0,0 +1,20 @@
+let slides = document.getElementsByClassName('slide')
+let dots = document.getElementsByClassName('dot')
+
+let slideIndex = 0
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  slideIndex++
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '')
+  }
+  slides[slideIndex - 1].style.display = 'block'
+  dots[slideIndex - 1].className += ' active'
+  setTimeout(showSlides, 5000)
+}
+
+showSlides()
