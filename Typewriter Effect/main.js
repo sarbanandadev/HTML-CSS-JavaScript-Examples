11da330cccccc7035d264d643e55c147c7c8501c
sarbanandadev <sarbanandadev@gmail.com>
2021-12-03 10:59:47 +0600

    initial commit

---
+++ Typewriter Effect/main.js
@@ -0,0 +1,20 @@
+const textAll = document.querySelectorAll(".typewriter-effect p")
+let textNo = 1
+
+const removeTextClasses = () => {
+  textAll.forEach((t) => {
+    t.classList.remove("active")
+  })
+}
+
+textAll[0].classList.add("active")
+
+setInterval(() => {
+  removeTextClasses()
+  if (textNo > textAll.length - 1) {
+    textNo = 0
+  }
+
+  textAll[textNo].classList.add("active")
+  textNo++
+}, 4000)
